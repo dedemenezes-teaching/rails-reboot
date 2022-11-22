27050e542b758bf37c27adf11a9ca8b6b3894178
Andre Menezes <djdedemenezes@gmail.com>
2022-11-22 18:48:02 -0300

    creatingbooking worksssssssssss

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :football_fields, only: [:index, :show]
-  resources :bookings, only: [:new]
+  resources :football_fields, only: [:index, :show] do
+    resources :bookings, only: [:new, :create]
+  end
+  resources :bookings, only: :destroy
+
 end
